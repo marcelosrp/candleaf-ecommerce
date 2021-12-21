f41ea21f27dd676802c0fb6616f5e69b8475f4bd
Marcelo Soares <soares.srp@gmail.com>
2021-12-20 21:59:58 -0300

    função controladora media querie no header

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,3 +1,4 @@
+import { useState, useEffect } from 'react'
 import Logo from '@ui/Logo'
 import Nav from '@ui/Nav'
 import User from '@ui/User'
@@ -5,11 +6,32 @@ import User from '@ui/User'
 import * as S from './styles'
 
 export default function Header() {
+  const [isMobile, setIsMobile] = useState(false)
+
+  function isMobileFn() {
+    if (matchMedia('(max-width: 768px)').matches) {
+      setIsMobile(true)
+    } else {
+      setIsMobile(false)
+    }
+  }
+
+  useEffect(() => {
+    isMobileFn()
+    window.addEventListener('resize', isMobileFn)
+    window.addEventListener('load', isMobileFn)
+
+    return () => {
+      window.removeEventListener('resize', isMobileFn)
+      window.removeEventListener('load', isMobileFn)
+    }
+  }, [])
+
   return (
     <S.Header>
       <S.HeaderWrapper>
         <Logo />
-        <Nav />
+        {isMobile ? <p style={{ order: -1 }}>mobile</p> : <Nav />}
         <User />
       </S.HeaderWrapper>
     </S.Header>
--- src/components/Header/styles.js
+++ src/components/Header/styles.js
@@ -4,6 +4,10 @@ export const Header = styled.header`
   border-bottom: 1px solid var(--cinza2);
   height: 7.5rem;
   width: 100%;
+
+  @media (max-width: 767px) {
+    padding: 0 1.5rem;
+  }
 `
 
 export const HeaderWrapper = styled.div`
@@ -12,5 +16,6 @@ export const HeaderWrapper = styled.div`
   height: 100%;
   justify-content: space-between;
   margin: 0 auto;
+  max-width: 100%;
   width: var(--container);
 `
